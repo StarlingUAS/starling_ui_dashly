3e77fa005e7d9b585e126195673b807ad2b0f333
Mickey Li <mickeyhli@outlook.com>
2021-09-03 17:15:32 +0100

    Modified dockerfile to install starling_allocator_msgs and all topics now send Empty msgs

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,17 @@ FROM ros:foxy-ros-base-focal
 RUN apt-get update \
     && apt-get install --no-install-recommends -y \
         python3-pip \
+        git \
+        ros-foxy-mavros-msgs \
     && rm -rf /var/lib/apt/lists/
 
 RUN pip3 install dash dash-bootstrap-components gunicorn pandas
 
 RUN mkdir -p ros_ws/src
 
-COPY starling_ui_dashly ros_ws/src/
+RUN git clone https://github.com/mhl787156/starling_allocator_msgs.git /ros_ws/src/starling_allocator_msgs
+
+COPY starling_ui_dashly /ros_ws/src/starling_ui_dashly/
 
 COPY ros_entrypoint.sh /ros_entrypoint.sh
 
@@ -21,7 +25,7 @@ WORKDIR ros_ws
 
 RUN . /opt/ros/foxy/setup.sh \
     && export CMAKE_PREFIX_PATH=$AMENT_PREFIX_PATH:$CMAKE_PREFIX_PATH \
-    && colcon build --packages-select starling_ui_dashly --cmake-force-configure \
+    && colcon build --packages-select starling_ui_dashly starling_allocator_msgs --cmake-force-configure \
     && rm -r build
 
 EXPOSE 3000
--- starling_ui_dashly/starling_ui_dashly/node.py
+++ starling_ui_dashly/starling_ui_dashly/node.py
@@ -1,23 +1,28 @@
 import rclpy
 from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
+import numpy as np
 
-from std_msgs.msg import String
+from std_msgs.msg import Empty
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from starling_allocator_msgs.srv import AllocateTrajectories
+from builtin_interfaces.msg import Duration
 
 class Dashboard_Node(Node):
     def __init__(self):
         super().__init__('dashboard_node')
 
         # ROS Setup
-        self.mission_start_publisher_ = self.create_publisher(String, 'mission_start', 10)
-        self.mission_abort_publisher_ = self.create_publisher(String, 'mission_abort', 10)
-        self.emergency_stop_publisher_ = self.create_publisher(String, 'emergency_stop', 10)
+        self.mission_start_publisher_ = self.create_publisher(Empty, 'mission_start', 10)
+        self.mission_abort_publisher_ = self.create_publisher(Empty, 'mission_abort', 10)
+        self.emergency_stop_publisher_ = self.create_publisher(Empty, 'emergency_stop', 10)
+
+        self.trajectory_allocation_client_ = self.create_client(AllocateTrajectories, 'submit_trajectories')
         self.timer_period = 0.01  # seconds
         self.emergency_stop_timer = None
 
     def call_mission_start(self):
-        msg = String()
-        msg.data = 'mission_start'
+        msg = Empty()
         self.mission_start_publisher_.publish(msg)
         self.get_logger().info('mission_start published')
 
@@ -31,13 +36,34 @@ class Dashboard_Node(Node):
         self.get_logger().info(f"ESTOP {'ENABLED' if send else 'DISABLED'}")
 
     def send_emergency_stop(self):
-        msg = String()
-        msg.data = 'emergency_stop'
+        msg = Empty()
         self.emergency_stop_publisher_.publish(msg)
         self.get_logger().info('emergency_stop published')
 
     def call_mission_abort(self):
-        msg = String()
-        msg.data = 'mission_abort'
+        msg = Empty()
         self.mission_abort_publisher_.publish(msg)
         self.get_logger().info('mission_abort published')
+
+    def send_trajectory_allocation(self, trajectory_dict):
+        self.get_logger().info('sending allocate trajectory service request')
+
+        while not self.trajectory_allocation_client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+
+        request = AllocateTrajectories.request()
+
+        for traj in trajectory_dict:
+            jt = JointTrajectory()
+            jt.points = []
+            for p in traj['data']:
+                jtp = JointTrajectoryPoint()
+                d = Duration()
+                time = p['time']
+                d.sec = int(np.floor(time))
+                d.nanosec = int((time - d.sec) * 10e9)
+                jtp.time_from_start = d
+                jtp.positions = [p['x'], p['y'], p['z']]
+                jt.points.append(jtp)
+
+        self.trajectory_allocation_client_.call_async(request)
